7be8b5402971a6c32af568ce6b2f0c45768afa46
Danny Viasus(Danjavia) <danny.viasus@comparamejor.com>
2016-02-09 14:23:09 -0500

    header

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -7,7 +7,8 @@ export default class Header extends Component {
 		super( props )
 		
 		this.state = {
-			open: false
+			open: false,
+			showModalMobile: false
 		}
 
 	}
@@ -16,40 +17,32 @@ export default class Header extends Component {
     	return `navbar-collapse ${ (( value === true ) ? 'collapse in': 'default' ) }`
   	}
 
+  	showModalMobile () {
+  		this.setState({ showModalMobile: true })
+  	}	
+
 	render() {
 		return (
 			<header id="comparamejor-main-header" className="rastreator comparamejor-main-header">
 				<nav className="navbar navbar-default comparamejor-main-header__navbar" role="navigation">
-					<div className="container">
+					<div className="container comparamejor-main-header__container">
 						{/*Brand and toggle get grouped for better mobile display */}
 						<div className="navbar-header comparamejor-main-header__navbar-header">
-							<button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#header-navbar-collapse" onClick={ ()=> this.setState({ open: ! this.state.open })}>
-								<span className="sr-only">Toggle navigation</span>
-								<span className="icon-bar"></span>
-								<span className="icon-bar"></span>
-								<span className="icon-bar"></span>
-							</button>
 							<a className="navbar-brand comparamejor-main-header__navbar-brand" href="https://comparamejor.com/">
-								<img src="https://segdig1.s3.amazonaws.com/static/bower_components/frontendquillo/images/main/header-logo.png" height="72px" width="auto" alt="ComparaMejor.com"/>
+								<img src="https://segdig1.s3.amazonaws.com/static/bower_components/frontendquillo/images/main/ComparaMejor.svg" height="72px" width="auto" alt="ComparaMejor.com"/>
 							</a>
 						</div>
 
-						<div className={ this.isActive( this.state.open ) } id="header-navbar-collapse">
-							<div className="block top">
-								<div className="wrapper clearfix">
-									<ul id="user-block" className="nav navbar-nav submenu-mobile">
-										<li id="account-button" className="dropdown">
-											<a href="/usuarios/datos-personales/" className="upper comparamejor-main-header__login"><i className="fa fa-user"></i> Mis Datos</a>
-										</li>
-									</ul>
-									{/*This is a requirement from Ricardo Buitrago due to an arrangement with inbox labs*/}
-									
-									<p id="header-phones" className="navbar-text contact-mobile">
-										<i className="fa fa-phone"></i> (1) 756 1234
-									</p>
-								</div>
-							</div>
-						</div>
+						<section className="right-section">
+							<button onClick={ this.showModalMobile.bind( this ) } className="btn btn-callme visible-xs">
+								<i className="fa fa-phone"></i> Llámame ya!
+							</button>
+		                    
+		                    <div className="hidden-xs contact">
+		                        <span className="first"><a href="tel:7561234"><i className="fa fa-phone"></i></a> (1) 756 1234</span>
+		                    </div>
+						</section>
+
 					</div>
 				</nav>
 			</header>
